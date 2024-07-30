407052ca3953224516015e60a249a74f8af7efe3
Daewoo Lee <lsinji@gmail.com>
2024-07-31 00:54:51 +0900

    내부 배포 추가

--- apps/paradise-calculator/next.config.mjs
+++ apps/paradise-calculator/next.config.mjs
@@ -1,9 +1,14 @@
+const prefix =
+  process.env.NODE_ENV === 'production' ? 'https://keep-ones.me' : ''
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  // assetPrefix: prefix
 };
 
 export default nextConfig;
